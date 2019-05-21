f304338d7615def41cf113746da3325cb4bcb554
Joerg Schmitz <joerg.schmitz@rewe-group.com>
2019-05-21 17:57:06 +0200

    Several improvements

--- src/app/pie-chart/pie-chart.page.ts
+++ src/app/pie-chart/pie-chart.page.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PieChartPage implements OnInit {
   title = 'Google Pie Chart with Ionic 4';
+  type = 'PieChart';
+  data = [
+    ['Firefox', 45.0],
+    ['IE', 26.8],
+    ['Chrome', 12.8],
+    ['Safari', 8.5],
+    ['Opera', 6.2],
+    ['Others', 0.7]
+  ];
+  columnNames = ['Browser', 'Percentage'];
+  options = {
+    colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'], is3D: true
+  };
+  width = 550;
+  height = 400;
 
   constructor() { }
 
